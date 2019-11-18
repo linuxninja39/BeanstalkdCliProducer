7ebaf870aa80ba310349d4afa5dd318c464c7ab4
Jacob Boswell <jacob@s56.net>
2019-11-18 11:54:29 -0700

    first commit

---
+++ BeanstalkdCliProducer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using EarthDiverDtos;
+using EarthDiverDtos.Enums;
+using Turbocharged.Beanstalk;
+
+namespace BeanstalkdCliProducer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var imageResizeRequest = new ImageResizeRequestDto
+            {
+                Name = "bob"
+            };
+            var beanstalkdClient = await BeanstalkConnection.ConnectProducerAsync("localhost:11300");
+            await beanstalkdClient.UseAsync(JobChannelsEnum.ImageResize.ToString());
+            await beanstalkdClient.PutAsync(imageResizeRequest, 5, TimeSpan.Zero);
+        }
+    }
+}
