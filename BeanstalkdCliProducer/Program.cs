2904058c04e7ceeaf041c4eb7e8f51cc9dab46c6
Jacob Boswell <jacob@s56.net>
2019-12-18 17:02:01 -0700

    added more stuff to json

--- BeanstalkdCliProducer/Program.cs
+++ BeanstalkdCliProducer/Program.cs
@@ -12,7 +12,10 @@ namespace BeanstalkdCliProducer
         {
             var imageResizeRequest = new ImageResizeRequestDto
             {
-                Name = "bob"
+                ClientId = "e552ed0c-5dde-475a-983e-ef3e4defb700",
+                Id = 548667,
+                Uri = new Uri("http://digitalcommunications.wp.st-andrews.ac.uk/files/2019/04/JPEG_compression_Example-768x576.jpg"),
+                Name = "Test"
             };
             var beanstalkdClient = await BeanstalkConnection.ConnectProducerAsync("localhost:11300");
             await beanstalkdClient.UseAsync(JobChannelsEnum.ImageResize.ToString());
